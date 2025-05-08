50eb8698b6a8a0983dd329ab5b8ce56d3195c680
mariopellicer <marioopelli23@gmail.com>
2025-05-08 09:52:50 +0200

    Clases Cliente y Pedido creadas

--- Reto3MarioPilar/src/clases/Cliente.java
+++ Reto3MarioPilar/src/clases/Cliente.java
@@ -1,5 +1,48 @@
 package clases;
 
 public class Cliente {
+private int idcliente;
+private String nombre;
+private String direccion;
+private int codigo;
+public Cliente() {
+	super();
+}
+public Cliente(int idCliente, String nombre, String direccion, int codigo) {
+	super();
+	this.idcliente = idCliente;
+	this.nombre = nombre;
+	this.direccion = direccion;
+	this.codigo = codigo;
+}
+public int getIdCliente() {
+	return idcliente;
+}
+public void setIdCliente(int idCliente) {
+	this.idcliente = idCliente;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+public int getCodigo() {
+	return codigo;
+}
+public void setCodigo(int codigo) {
+	this.codigo = codigo;
+}
+@Override
+public String toString() {
+	return "Cliente [idCliente=" + idcliente + ", nombre=" + nombre + ", direccion=" + direccion + ", codigo=" + codigo
+			+ "]";
+}
 
 }
--- Reto3MarioPilar/src/clases/Pedido.java
+++ Reto3MarioPilar/src/clases/Pedido.java
@@ -1,5 +1,58 @@
 package clases;
 
+import java.util.Date;
+
 public class Pedido {
+private int idpedido;
+private Cliente cliente;
+private double precioTotal;
+private String direccionEnvio;
+private Date fecha;
+public Pedido() {
+	super();
+}
+public Pedido(int idpedido, Cliente cliente, double precioTotal, String direccionEnvio, Date fecha) {
+	super();
+	this.idpedido = idpedido;
+	this.cliente = cliente;
+	this.precioTotal = precioTotal;
+	this.direccionEnvio = direccionEnvio;
+	this.fecha = fecha;
+}
+public int getIdpedido() {
+	return idpedido;
+}
+public void setIdpedido(int idpedido) {
+	this.idpedido = idpedido;
+}
+public Cliente getCliente() {
+	return cliente;
+}
+public void setCliente(Cliente cliente) {
+	this.cliente = cliente;
+}
+public double getPrecioTotal() {
+	return precioTotal;
+}
+public void setPrecioTotal(double precioTotal) {
+	this.precioTotal = precioTotal;
+}
+public String getDireccionEnvio() {
+	return direccionEnvio;
+}
+public void setDireccionEnvio(String direccionEnvio) {
+	this.direccionEnvio = direccionEnvio;
+}
+public Date getFecha() {
+	return fecha;
+}
+public void setFecha(Date fecha) {
+	this.fecha = fecha;
+}
+@Override
+public String toString() {
+	return "Pedido [idpedido=" + idpedido + ", cliente=" + cliente + ", precioTotal=" + precioTotal
+			+ ", direccionEnvio=" + direccionEnvio + ", fecha=" + fecha + "]";
+}
 
 }
