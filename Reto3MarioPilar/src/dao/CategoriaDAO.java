f15c1c207b3f9fc7cf70423a4cc4746e504387ea
mariopellicer <marioopelli23@gmail.com>
2025-05-12 12:40:54 +0200

    funciones Alta de nuevos y gestion categorias creada en clienteDAO y CategoriaDAO

--- Reto3MarioPilar/src/dao/CategoriaDAO.java
+++ Reto3MarioPilar/src/dao/CategoriaDAO.java
@@ -1,5 +1,31 @@
 package dao;
 
-public class CategoriaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-}
+import clases.Categoria;
+import util.Conexion;
+
+public class CategoriaDAO {
+	public static void gestionCategorias(Categoria categoria) {
+		 try(Connection con = Conexion.abreConexion())
+		 {
+			PreparedStatement st = con.prepareStatement("insert into categorias(nombre) values(?)",Statement.RETURN_GENERATED_KEYS);
+		 	st.setString(1,categoria.getNombre());
+			st.executeUpdate();
+		 	ResultSet rs = st.getGeneratedKeys();
+		 	if (rs.next()) {
+		 		categoria.setIdCategoria(rs.getInt(1));
+		 	}
+		 	rs.close();
+		 }catch (Exception ex){
+		 	ex.printStackTrace();
+		 }
+		 finally {
+			Conexion.cierraConexion();
+		}
+	 }
+	}
+
--- Reto3MarioPilar/src/dao/ClienteDAO.java
+++ Reto3MarioPilar/src/dao/ClienteDAO.java
@@ -1,5 +1,33 @@
 package dao;
 
-public class ClienteDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-}
+import clases.Cliente;
+import util.Conexion;
+
+public class ClienteDAO {
+	public static void AltaDeNuevos(Cliente cliente) {
+		 try(Connection con = Conexion.abreConexion())
+		 {
+			PreparedStatement st = con.prepareStatement("insert into categorias(nombre,direccion,codigo) values(?,?,?)",Statement.RETURN_GENERATED_KEYS);
+		 	st.setString(1,cliente.getNombre());
+		 	st.setString(2,cliente.getDireccion());
+		 	st.setInt(3,cliente.getCodigo());
+			st.executeUpdate();
+		 	ResultSet rs = st.getGeneratedKeys();
+		 	if (rs.next()) {
+		 		cliente.setIdCliente(rs.getInt(1));
+		 	}
+		 	rs.close();
+		 }catch (Exception ex){
+		 	ex.printStackTrace();
+		 }
+		 finally {
+			Conexion.cierraConexion();
+		}
+	 }
+	}
+
