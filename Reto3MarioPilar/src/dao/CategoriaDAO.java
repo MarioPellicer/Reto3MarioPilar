1dcccc90747866c13372575fc5df5dc41eb2683c
JorgeRuedaP <jorgeruedaplo2004@gmail.com>
2025-05-13 09:44:51 +0200

    select categoria

--- Reto3MarioPilar/src/dao/CategoriaDAO.java
+++ Reto3MarioPilar/src/dao/CategoriaDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import clases.Categoria;
+import clases.Producto;
 import util.Conexion;
 
 public class CategoriaDAO {
@@ -27,5 +30,24 @@ public class CategoriaDAO {
 			Conexion.cierraConexion();
 		}
 	 }
+	
+	public static List<Categoria> listaCategoria() {
+		List<Categoria> lista = new ArrayList<Categoria>();
+		try {
+			Connection con = Conexion.abreConexion();
+			PreparedStatement pst = con.prepareStatement("SELECT idCategoria, nombre FROM categoria");
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				lista.add(new Categoria(rs.getInt("idCategoria"),rs.getString("nombre")));
+			}
+			rs.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Conexion.cierraConexion();
+		}
+		return lista;
 	}
+}
 
