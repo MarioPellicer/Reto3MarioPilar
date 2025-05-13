6dbb5195b07085c4fb282b961c2cb09590a5844b
mariopellicer <marioopelli23@gmail.com>
2025-05-13 08:36:54 +0200

    Merge branch 'main' of https://github.com/MarioPellicer/Reto3MarioPilar.git into main creada la funcion verPedidos en pedidoDAO

--- Reto3MarioPilar/src/dao/PedidoDAO.java
+++ Reto3MarioPilar/src/dao/PedidoDAO.java
@@ -3,12 +3,38 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 
 import clases.Cliente;
 import clases.Pedido;
 import util.Conexion;
 
 public class PedidoDAO {
+	public static Pedido verPedidos(Pedido pedido) {
+		 Pedido pedidoNuevo = new Pedido();
+		 LocalDate ls= LocalDate.now();
+		 ls.getMonth();
+		 try(Connection con = Conexion.abreConexion())		 
+		 {
+		 	PreparedStatement stmt = con.prepareStatement("select * \r\n"
+		 			+"from pedido \r\n " 
+		 			+ " where month(fecha) = month(curdate()); " );
+		 	ResultSet rs = stmt.executeQuery();
+		 	if(rs.next())
+		 	{    
+		 		pedidoNuevo=new Pedido(rs.getInt("idPedido"),new Cliente(),rs.getDouble("precioTotal"),rs.getString("direccionEnvio"),rs.getDate("fecha"));
+		 	}else {
+				return null;
+			}
+		 	rs.close();
+		 }catch (Exception ex){
+		 	ex.printStackTrace();
+		 }
+		 finally {
+			Conexion.cierraConexion();
+		}
+		return pedidoNuevo;
+	}
 	public static Pedido buscarCliente(Pedido pedido){
 		 Pedido pedidoNuevo = new Pedido();
 		 try(Connection con = Conexion.abreConexion())
